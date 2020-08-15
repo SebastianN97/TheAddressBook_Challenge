5dee7bb14f79c8a1b94315130b0721bd149d3b00
SebastianN97 <sebbe132132@gmail.com>
2020-08-15 19:43:35 +0200

    Modified Integrations with two new It and html with div and script

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -4,12 +4,23 @@ describe('user can create a contact', () => {
         cy.visit('http://localhost:3001')
         cy.get('#add-contact').click()
 
+        // Adding Thomas to the address book
         cy.get('#name').type('Thomas')
 		cy.get('#email').type('thomas@craft.se')
 		cy.get('#phone').type('0700 101010')
 		cy.get('#company').type('Craft Academy')
 		cy.get('#notes').type('Awesome coder')
-		cy.get('#twitter').type('@thomasochman')
+        cy.get('#twitter').type('@thomasochman')
+        cy.get('#submit').click()
+
+    })
+
+	it('displays a name of the new contact', () => {
+		cy.get('#contact-list').should('contain', 'Thomas')
+	})
+	
+	it('displays the phone number of the new contact', () => {
+		cy.get('#contact-list').should('contain', '0700 101010')
     })
 })
 
